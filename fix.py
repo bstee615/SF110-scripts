b0f0ccb29caea6c866b7fb5719866d49cc6beb40
bstee615 <benjaminjsteenhoek@gmail.com>
2024-02-08 15:03:06 -0600

    Add first version of build XML fix script

---
+++ fix.py
@@ -0,0 +1,22 @@
+from lxml import etree
+from pathlib import Path
+
+build_xmls = list(Path(".").glob("*_*/build.xml"))
+
+for xml in build_xmls:
+    # Load the XML file
+    tree = etree.parse(xml)
+    root = tree.getroot()
+
+    # Add a child element to <path> with id="test.lib"
+    path_element = root.find("./path[@id='test.lib']")
+    new_child = etree.Element("pathelement", location="${lib.dir}/hamcrest-core-1.3.jar")
+    path_element.append(new_child)
+
+    # Add a child element to <classpath> under the specified target
+    classpath_element = root.find("./target[@name='evosuite-test']/junit/classpath")
+    new_classpath_child = etree.Element("pathelement", path="${build.tests}")
+    classpath_element.append(new_classpath_child)
+
+    # Save the modified XML tree back to the file
+    tree.write(xml.parent/(xml.stem+"_fixed.xml"), encoding="utf-8", xml_declaration=True)
