3eef3ea95372f9c741c4544b81827b8ff67ab6ed
bstee615 <benjaminjsteenhoek@gmail.com>
2024-02-11 22:03:08 -0600

    Spruce up docker scripts

---
+++ docker_build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker build -t benjijang/sf110:latest .
+docker push benjijang/sf110:latest
--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 docker run --rm -it --name sf110 -v $PWD:/workspace benjijang/sf110:latest bash
